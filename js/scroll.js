7c9a400f2ce792f78b9b8a836bfb757a9b9c6b96
Grzegorz Sparzak <g.sparzak@gmail.com>
2016-05-19 15:48:15 +0200

    poprawienie kodu

--- js/scroll.js
+++ js/scroll.js
@@ -5,12 +5,12 @@ jQuery(function($)
     {
         //zresetuj scrolla
 
-        $('#link').click(function() { $.scrollTo($('#opis'), 1500); return false});
-        $('#link1').click(function() { $.scrollTo($('#zespol'), 1500); return false});
-        $('#link3').click(function() { $.scrollTo($('body'), 1000); return false});
-        $('#backtop').click(function() { $.scrollTo($('body'), 1000); return false});
-        $('#cont').click(function() { $.scrollTo($('#email'), 1000); return false});
-        $('.button').click(function() { $.scrollTo($('#opis'), 1000); return false});
+        $('#link').click(function() { $.scrollTo($('#opis'), 500); return false});
+        $('#link1').click(function() { $.scrollTo($('#zespol'), 500); return false});
+        $('#link3').click(function() { $.scrollTo($('body'), 500); return false});
+        $('#backtop').click(function() { $.scrollTo($('body'), 500); return false});
+        $('#cont').click(function() { $.scrollTo($('#email'), 500); return false});
+        $('.button').click(function() { $.scrollTo($('#opis'), 500); return false});
     }
 );
 
