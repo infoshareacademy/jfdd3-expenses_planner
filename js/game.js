b501ff301ab3eab4fcfe4d5386738b9d95f651d3
Dawid <dawid.kubic@gmail.com>
2016-05-23 10:22:17 +0200

    game change

--- js/game.js
+++ js/game.js
@@ -8,11 +8,11 @@ function createBoard(height, width) {
          $row = $("<tr>");
         for (var x = 0; x < width; x += 1) {
              $data = $('<td>')
-            .addClass('game_square');
+            .addClass('game_block');
             $row.append($data);
         }
         $board.append($row);
     }
     return $board;
 }
-$('#game').append(createBoard(15,15));
+$('#game').append(createBoard(10,10));
