034fe0580e0c73fdcbaa0a4d11369ee94cf38080
Krystian Konkol <konkolkrystian@gmail.com>
2016-05-23 10:23:04 +0200

    deleted empty spaces

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     $('a[href^="#"]').on('click', function(event) {
         var target = $( $(this).attr('href') );
-        if(target.length ) {
+        if(target.length) {
             event.preventDefault();
             $('html, body').animate({
                 scrollTop: target.offset().top
