bdeb16ea058fa428e6b26a4f0bfa9c66eadad84b
Krystian Konkol <konkolkrystian@gmail.com>
2016-05-25 16:12:25 +0200

    merge  with dev done

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-$('#sendForm').on('click', function() {
+$('#sendForm').on('click', function () {
     var $box,
         $gameBoard,
         $form = $("#email");
@@ -16,7 +16,7 @@ $('#sendForm').on('click', function() {
 
     $('#game').css({
 
-        height:"100vh"
+        height: "100vh"
     });
 
 });
@@ -30,7 +30,7 @@ function createBoard(height, width) {
         $row = $("<tr>");
         for (var x = 0; x < width; x += 1) {
             $data = $('<td>')
-                .addClass('cell').attr('x', x).attr('y',y);
+                .addClass('cell').attr('x', x).attr('y', y);
             $row.append($data);
         }
         $board.append($row);
@@ -38,15 +38,6 @@ function createBoard(height, width) {
     return $board;
 }
 
-
-
-
-
-
-
-
-/*
-$('#game').append(createBoard(10, 10));
 
 // Game constant options - movement
 
@@ -56,8 +47,8 @@ $('#game').append(createBoard(10, 10));
 // 5  4  3
 
 var DIR = {UP: 0, UPRIGHT: 1, RIGHT: 2, DOWNRIGHT: 3, DOWN: 4, DOWNLEFT: 5, LEFT: 6, UPLEFT: 7};
-var DIRX = [   0,          1,        1,            1,       0,          -1,      -1,        -1];
-var DIRY = [  -1,         -1,        0,            1,       1,           1,       0,        -1];
+var DIRX = [0, 1, 1, 1, 0, -1, -1, -1];
+var DIRY = [-1, -1, 0, 1, 1, 1, 0, -1];
 
 var Point = function (x, y, dir) {
     this.x = x + (DIRX[dir] || 0);
@@ -67,50 +58,56 @@ var Point = function (x, y, dir) {
 //var dir = DIR.UP;
 //var newPosition = {x : DIRX[dir], y: DIRX[dir] }
 
-function horizontal(dir) { return (dir === DIR.LEFT) || (dir === DIR.RIGHT)}
-function vertical(dir) {return (dir === DIR.UP) || (dir === DIR.DOWN)}
+function horizontal(dir) {
+    return (dir === DIR.LEFT) || (dir === DIR.RIGHT)
+}
+function vertical(dir) {
+    return (dir === DIR.UP) || (dir === DIR.DOWN)
+}
 
-var arrow = {left: 37, up: 38, right: 39, down:40 };
+var arrow = {left: 37, up: 38, right: 39, down: 40};
 var handled = false;
 
-get: function(p, dir) { return this.cells[p.x + (DIRX[dir]) || 0][p.y + (DIRY[dir] || 0)].object;},
+get: function (p, dir) {
+    return this.cells[p.x + (DIRX[dir]) || 0][p.y + (DIRY[dir] || 0)].object;
+}
+,
 
 
 //Sprite IMG and code
-
 
 
 //Player Script
 
 $(document).keydown(function (ev) {
     var keyCode = ev.keyCode || ev.which;
-   switch (ev.keyCode) {
-       case arrow.left:
-           moving.startLeft();
-           handled = true;
-           console.log('left');
-           break;
-       case arrow.up:
-           moving.startUp();
-           handled = true;
-           console.log('up');
-           break;
-       case arrow.right:
-           console.log('right');
-           moving.startRight();
-           handled = true;
-           break;
-       case arrow.down:
-           console.log('down');
-           moving.startDown();
-           handled = true;
-           break;
-   }
+    switch (ev.keyCode) {
+        case arrow.left:
+            moving.startLeft();
+            handled = true;
+            console.log('left');
+            break;
+        case arrow.up:
+            moving.startUp();
+            handled = true;
+            console.log('up');
+            break;
+        case arrow.right:
+            console.log('right');
+            moving.startRight();
+            handled = true;
+            break;
+        case arrow.down:
+            console.log('down');
+            moving.startDown();
+            handled = true;
+            break;
+    }
     if (handled)
         ev.preventDefault();
 });
 
-$(document).keyup(function (ev){
+$(document).keyup(function (ev) {
         switch (ev.keyCode) {
             case arrow.left:
                 moving.stopLeft();
@@ -133,7 +130,7 @@ $(document).keyup(function (ev){
                 handled = true;
                 break;
         }
-}
+    }
 );
 
 //Player Movement
@@ -141,16 +138,40 @@ $(document).keyup(function (ev){
 var moving = {
     dir: DIR.NONE,
     lastXDir: DIR.NONE,
-    up: false, down: false, left:false, right:false,
-    startUp:    function() {this.up     = true; this.dir = DIR.UP;},
-    startDown:  function() {this.down   = true; this.dir = DIR.DOWN;},
-    startLeft:  function() {this.left   = true; this.dir = DIR.LEFT;},
-    startRight: function() {this.right  = true; this.dir = DIR.RIGHT;},
-    stopUp:     function() {this.up     = false; this.dir = (this.dir == DIR.UP) ? this.where() : this.dir;},
-    stopDown:   function() {this.down   = false; this.dir = (this.dir == DIR.DOWN) ? this.where() : this.dir;},
-    stopRight:  function() {this.right  = false; this.dir = (this.dir == DIR.RIGHT) ? this.where() : this.dir;},
-    stopLeft:   function() {this.left   = false; this.dir = (this.dir == DIR.LEFT) ? this.where() : this.dir;},
-    where: function(){
+    up: false, down: false, left: false, right: false,
+    startUp: function () {
+        this.up = true;
+        this.dir = DIR.UP;
+    },
+    startDown: function () {
+        this.down = true;
+        this.dir = DIR.DOWN;
+    },
+    startLeft: function () {
+        this.left = true;
+        this.dir = DIR.LEFT;
+    },
+    startRight: function () {
+        this.right = true;
+        this.dir = DIR.RIGHT;
+    },
+    stopUp: function () {
+        this.up = false;
+        this.dir = (this.dir == DIR.UP) ? this.where() : this.dir;
+    },
+    stopDown: function () {
+        this.down = false;
+        this.dir = (this.dir == DIR.DOWN) ? this.where() : this.dir;
+    },
+    stopRight: function () {
+        this.right = false;
+        this.dir = (this.dir == DIR.RIGHT) ? this.where() : this.dir;
+    },
+    stopLeft: function () {
+        this.left = false;
+        this.dir = (this.dir == DIR.LEFT) ? this.where() : this.dir;
+    },
+    where: function () {
         if (this.up)
             return DIR.UP;
         else if (this.down)
