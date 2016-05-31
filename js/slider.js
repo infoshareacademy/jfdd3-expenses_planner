9c5643f0e10cc40fbe0249f8c32903fe38f4db3f
Grzegorz Sparzak <g.sparzak@gmail.com>
2016-05-31 16:27:17 +0200

    slider ready

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,9 @@
 'use strict';
 
+var $slides = $('.member');
+
 $('#left_scroll').click(function (){
     $slides.filter('.visible').removeClass('visible').prev().addClass('visible');
-    //order -= 1;
     if ($slides.filter('.visible').length === 0) {
         $slides.last().addClass('visible');
     }
@@ -10,30 +11,12 @@ $('#left_scroll').click(function (){
 
 $('#right_scroll').click(function (){
     $slides.filter('.visible').removeClass('visible').next().addClass('visible');
-    //order += 1;
     if ($slides.filter('.visible').length === 0) {
         $slides.first().addClass('visible');
     }
 });
 
-var $slides = $('.member');
-//var order = 1;
-
 setInterval(function(){
-    //if (order === 0){
-    //    $slides.eq(3).removeClass('visible');
-    //}
-    //else {
-    //    $slides.eq(order-1).removeClass('visible');
-    //}
-    //
-    //$slides.eq(order).addClass('visible');
-    //
-    //if(order === 3) {
-    //    order = 0;
-    //}else {
-    //    order++;
-    //}
     $('#right_scroll').click();
 }, 3000);
 
