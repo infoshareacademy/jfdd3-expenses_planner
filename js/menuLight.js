18bd19864eb2aa402cf4bf3c3581941e99701c98
nataliawaligora <fliberczibit@gmail.com>
2016-05-31 23:29:01 +0200

    Nowy formularz zapisowy 23:28

--- js/menuLight.js
+++ js/menuLight.js
@@ -1,11 +1,28 @@
-
 'use strict';
 
-$(document).ready(function () {
-    $('.zajawa').click(function () {
-        $(this).find('a').css({
-            color:"orange"
-        })
-    });
+//
+// $(document).ready(function () {
+//     $('.zajawa').click(function () {
+//         if ($(this).addClass('orange')) {
+//             $('.zajawa.orange').removeClass('orange')
+//         }
+//     })});
+// });
+// $(document).ready(function () {
+//     $('.zajawa').click(function () {
+//         $(this).addClass('orange');
+//         $('.zajawa.orange').click(function () {
+//             $(this).removeClass('orange');
+//         });
+//     });
+//
+//
+// });
 
-});
+// if (keyCode === 37) {
+//     if ($player.attr('x') > 0){
+//         $player.removeClass('player').addClass('black');
+//         $player = moves.left($player);
+//         $player.removeClass('diament').addClass('player');
+//     }
+// }
