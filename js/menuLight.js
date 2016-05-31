69b0746064c9581572b4fbf7afb3fef234611ef2
Natalia Waligora <fliberczibit@gmail.com>
2016-05-31 16:26:38 +0200

    menu ligh

---
+++ js/menuLight.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.zajawa').click(function () {
+        $(this).css({
+            color:"black"
+        })
+    });
+    console.log("hello");
+});
+
+
